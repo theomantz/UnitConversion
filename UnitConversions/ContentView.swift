297092fe2c2f68d379a03dfe6c96b6dbb6624354
theo m <theo@mantz.io>
2021-07-01 17:18:17 -0400

    Initial Commit

---
+++ UnitConversions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversions
+//
+//  Created by Theodore Mantz on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
